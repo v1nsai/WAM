04c17deae4d825fc4f14d678265a16e122372680
v1nsai <jamzsabb@gmail.com>
2015-05-20 15:06:51 -0500

    Initial commit

---
+++ app/src/main/java/com/rileinc/wam/WAM_View.java
@@ -0,0 +1,18 @@
+package com.rileinc.wam;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet; import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by v1nsai on 5/19/15.
+ */
+public class WAM_View extends SurfaceView implements SurfaceHolder.Callback {
+
+}
